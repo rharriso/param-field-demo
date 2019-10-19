897a6d235dc46821a9909939d360887590238a7b
Ross Harrison <rtharrison86@gmail.com>
2019-10-19 15:02:11 -0500

    Load graphql data into Client application

--- src-ui/app.tsx
+++ src-ui/app.tsx
@@ -1,14 +1,44 @@
 import * as React from 'react';
 import { render } from 'react-dom'
 
-import { ApolloProvider } from 'react-apollo';
+import { ApolloProvider, Query } from 'react-apollo';
 
 import apolloClient from 'src-ui/apolloClient';
+import gql from 'graphql-tag';
+import { imageQuery } from './schemaTypes/imageQuery';
+
+const imageQueryGQL = gql`
+query imageQuery {
+ 	fetchImage {
+ 	  title
+ 	  url
+ 	}
+}
+`;
 
 class AppRoot extends React.Component {
   public render() {
     return (
-      <h1>Hello</h1>
+      <Query<imageQuery> query={imageQueryGQL}>
+        {({ loading, data }) => {
+          if (loading) {
+            return <h1>Loading</h1>;
+          }
+
+          if (!data) {
+            return <h1>No Data Returned</h1>;
+          }
+
+          const { url, title } = data.fetchImage;
+
+          return (
+            <React.Fragment>
+              <img src={url} alt={title} />
+              <h2>{title}</h2>
+            </React.Fragment>
+          )
+        }}
+      </Query>
     );
   }
 }
---
+++ src-ui/schemaTypes/imageQuery.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: imageQuery
+// ====================================================
+
+export interface imageQuery_fetchImage {
+  __typename: "Image";
+  title: string;
+  url: string;
+}
+
+export interface imageQuery {
+  fetchImage: imageQuery_fetchImage;
+}
